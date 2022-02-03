3667f2731ce05288a966c6bc66fce88aa145faf3
dimaswsss <mas.dimas2910@gmail.com>
2022-02-03 23:03:28 +0700

    update belajar.py

--- belajar.py
+++ belajar.py
@@ -1,6 +1,9 @@
-print('Hello World')
 angka = 12
 if (angka == 12):
     print('angka ini jumlahnya 12')
 else:
     print('bodoh')
+name = "dimas"
+while (name == 'dimas'):
+    name = input('masukkan nama: ')
+    print('halo', name)
