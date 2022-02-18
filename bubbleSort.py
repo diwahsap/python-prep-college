0967d98d0634fad51d9a2936382a2a32d80d5c69
dimaswsss <mas.dimas2910@gmail.com>
2022-02-18 23:44:46 +0700

    berhasil bubble

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,11 +1,10 @@
 # bubble sort
 number = list(map(int, input("Masukkan angka: ").split()))
-def bubbleSort(number):
-    for i in range(len(number)):
-        for j in range(len(number) - 1):
-            if number[j] > number[j+1]:
-                # tukar number[j] dengan number[j+1]
-                number[j] = number[j+1]
-                number[j+1] = number[j]
-    return number
-print(bubbleSort(number))
+for i in range(len(number)):
+    for j in range(len(number) - 1):
+        if number[j] > number[j+1]:
+            # tukar number[j] dengan number[j+1]
+            temp = number[j]
+            number[j] = number[j+1]
+            number[j+1] = temp
+print(number)
