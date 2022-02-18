9523e1f50de7e230bd7214bf6641bc1c66c5b62d
dimaswsss <mas.dimas2910@gmail.com>
2022-02-18 23:42:16 +0700

    add gitginore

--- cobaCoPilot.py
+++ cobaCoPilot.py
@@ -1,1 +1,13 @@
-print('halo')
+# selection sort
+number = list(map(int, input("Masukkan angka: ").split()))
+for i in range(len(number)):
+    # cari elemen terkecil di list
+    imin = i
+    for j in range(i+1, len(number)):
+        if number[j] < number[imin]:
+            imin = j
+    # tukar elemen terkecil dengan elemen pertama
+    temp = number[i]
+    number[i] = number[imin]
+    number[imin] = temp
+print(number)
--- selectionSort.py
+++ selectionSort.py
@@ -2,7 +2,7 @@
 number = list(map(int, input("Masukkan angka: ").split()))
 for i in range(len(number)):
     # cari elemen terkecil di list
-    imin = i
+    imin = i # dianggap elemen terkecil
     for j in range(i+1, len(number)):
         if number[j] < number[imin]:
             imin = j
